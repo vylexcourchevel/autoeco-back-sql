322156d15aad9b5fed51eb6f7649b445fe7c2189
John Doe <vylex@hotmail.fr>
2024-11-26 15:57:36 +0100

    ajout    await connection.sync({ force: true  dans index.js

--- models/index.js
+++ models/index.js
@@ -95,7 +95,8 @@ const createAdminIfNeeded = async () => {
 // Synchroniser les modèles et créer l'administrateur
 (async () => {
   try {
-    await connection.sync();
+   // await connection.sync();
+   await connection.sync({ force: true });
 
     console.log('Base de données synchronisée');
 
@@ -104,6 +105,16 @@ const createAdminIfNeeded = async () => {
     console.error('Erreur lors de la synchronisation de la base de données :', error);
   }
 })();
+(async () => {
+    try {
+      console.log('Tentative de connexion à la base de données...');
+      await connection.authenticate();
+      console.log('Connexion réussie à la base de données');
+    } catch (error) {
+      console.error('Impossible de se connecter à la base de données :', error);
+    }
+  })();
+  
 
 export {
     Car,
@@ -113,7 +124,7 @@ export {
     User
 };
 
-// //index.js
+//index.js
 // import express from 'express';
 // import { Sequelize } from 'sequelize';
 // import dotenv from 'dotenv';  // Import dotenv pour charger les variables d'environnement
@@ -208,103 +219,100 @@ export {
 
 
 
-// // TEST RAJOUT variable d'environnement 
+// // // TEST RAJOUT variable d'environnement 
 
 
-// // src/models/index.js
+// // // src/models/index.js
 
-// import express from 'express';
-// //import multer from 'multer';
+// // import express from 'express';
+// // //import multer from 'multer';
 
-// import { Sequelize } from 'sequelize';
-// import carModel from './car.js';
-// import carImageModel from './carImage.js';
-// // Importer d'autres modèles si nécessaire
-// import paymentModel from './payment.js';
-// import reservationModel from './reservation.js';
-// import userModel from './user.js';
+// // import { Sequelize } from 'sequelize';
+// // import carModel from './car.js';
+// // import carImageModel from './carImage.js';
+// // // Importer d'autres modèles si nécessaire
+// // import paymentModel from './payment.js';
+// // import reservationModel from './reservation.js';
+// // import userModel from './user.js';
 
-// // Initialiser l'application Express
-// const app = express();
-// const PORT = process.env.PORT;
+// // // Initialiser l'application Express
+// // const app = express();
+// // const PORT = process.env.PORT;
 
-// // Nouvelle connexion à la base de données
-// const connection = new Sequelize(
-//     'autoeco', // Nom de la base de données
-//     'root', // Identifiant MySQL
-//     '', // Mot de passe MySQL
-//     {
-//         host: 'localhost', // URL de MySQL
-//         dialect: 'mysql', // Type de base de données
-//        // logging: console.log, // Active les logs SQL
-//     }
-// );
+// // // Nouvelle connexion à la base de données
+// // const connection = new Sequelize(
+// //     'autoeco', // Nom de la base de données
+// //     'root', // Identifiant MySQL
+// //     '', // Mot de passe MySQL
+// //     {
+// //         host: 'localhost', // URL de MySQL
+// //         dialect: 'mysql', // Type de base de données
+// //        // logging: console.log, // Active les logs SQL
+// //     }
+// // );
 
-// // Authentification et connexion à la base de données
-// try {
-//     await connection.authenticate();
-//     //await connection.sync({ force: true });
-// } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-// }
+// // // Authentification et connexion à la base de données
+// // try {
+// //     await connection.authenticate();
+// //     //await connection.sync({ force: true });
+// // } catch (error) {
+// //     console.error('Unable to connect to the database:', error);
+// // }
 
-// // Création des connexions aux tables via les modèles
-// carModel(connection, Sequelize);
-// carImageModel(connection, Sequelize);
-// // carLocationModel(connection, Sequelize);
-// // carTypeModel(connection, Sequelize);
-// paymentModel(connection, Sequelize);
-// reservationModel(connection, Sequelize);
-// userModel(connection, Sequelize);
+// // // Création des connexions aux tables via les modèles
+// // carModel(connection, Sequelize);
+// // carImageModel(connection, Sequelize);
+// // // carLocationModel(connection, Sequelize);
+// // // carTypeModel(connection, Sequelize);
+// // paymentModel(connection, Sequelize);
+// // reservationModel(connection, Sequelize);
+// // userModel(connection, Sequelize);
 
-// // Récupération des modèles créés
-// const {
-//     Car,
-//     CarImage,
-//     // CarLocation,
-//     // CarType,
-//     Payment,
-//     Reservation,
-//     User
-// } = connection.models;
+// // // Récupération des modèles créés
+// // const {
+// //     Car,
+// //     CarImage,
+// //     // CarLocation,
+// //     // CarType,
+// //     Payment,
+// //     Reservation,
+// //     User
+// // } = connection.models;
 
-// // Définition des relations entre les modèles
-// User.hasMany(Reservation);
-// Reservation.belongsTo(User);
+// // // Définition des relations entre les modèles
+// // User.hasMany(Reservation);
+// // Reservation.belongsTo(User);
 
-// Reservation.hasMany(Payment);
-// Payment.belongsTo(Reservation);
+// // Reservation.hasMany(Payment);
+// // Payment.belongsTo(Reservation);
 
-// Payment.belongsTo(User);
-// User.hasMany(Payment);
+// // Payment.belongsTo(User);
+// // User.hasMany(Payment);
 
-// Car.hasMany(CarImage);
-// CarImage.belongsTo(Car);
+// // Car.hasMany(CarImage);
+// // CarImage.belongsTo(Car);
 
-// Car.hasMany(Reservation);
-// Reservation.belongsTo(Car);
+// // Car.hasMany(Reservation);
+// // Reservation.belongsTo(Car);
 
-// await connection.sync();
-//  //await connection.sync({alter: true});
+// // await connection.sync();
+// //  //await connection.sync({alter: true});
 
-// // Démarrage du serveur
-// // app.listen(PORT, () => {
-// //     console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
-// // });
+// // // Démarrage du serveur
+// // // app.listen(PORT, () => {
+// // //     console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+// // // });
 
-// // Exportation des modèles pour utilisation dans d'autres parties de l'application
-// export {
-//     Car,
-//     CarImage,
-//     // CarLocation,
-//     // CarType,
-//     Payment,
-//     Reservation,
-//     User
-// };
-
-
-
+// // // Exportation des modèles pour utilisation dans d'autres parties de l'application
+// // export {
+// //     Car,
+// //     CarImage,
+// //     // CarLocation,
+// //     // CarType,
+// //     Payment,
+// //     Reservation,
+// //     User
+// // };
 
 
 
@@ -318,71 +326,74 @@ export {
 
 
 
-// import { Sequelize } from "sequelize";
-//  import carModel from "./car.js";
-//  import carImageModel from "./carImage.js";
-// // import carLocationModel from "./carLocation.js";
-// // import carTypeModel from "./carType.js";
-// import paymentModel from "./payment.js";
-// import reservationModel from "./reservation.js";
-// import userModel from "./user.js";
 
-// // Nouvelle connexion à la base de données
-// const connection = new Sequelize(
-//     'autoeco', // Nom de la base de données
-//     'root', // Identifiant MySQL
-//     '', // Mot de passe MySQL
-//     {
-//         host: 'localhost', // URL de MySQL
-//         dialect: 'mysql' // Type de base de données
-//     }
-// );
 
-// // Authentification et connexion à la base de données
-// try {
-//     await connection.authenticate();
-//     console.log('Connection has been established successfully.');
-// } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-// }
 
-// // Création des connexions aux tables via les modèles
-//  carModel(connection, Sequelize);
-//  carImageModel(connection, Sequelize);
-// // carLocationModel(connection, Sequelize);
-// // carTypeModel(connection, Sequelize);
-//  paymentModel(connection, Sequelize);
-//  reservationModel(connection, Sequelize);
-//  userModel(connection, Sequelize);
+// // import { Sequelize } from "sequelize";
+// //  import carModel from "./car.js";
+// //  import carImageModel from "./carImage.js";
+// // // import carLocationModel from "./carLocation.js";
+// // // import carTypeModel from "./carType.js";
+// // import paymentModel from "./payment.js";
+// // import reservationModel from "./reservation.js";
+// // import userModel from "./user.js";
 
-// // Récupération des modèles créés
-// const {
-//      Car,
-//      Payment,
-//      Reservation,
-//      User,
-//      CarImage
-//       // CarImage,
-//  // CarLocation,
-//  // CarType,
-// } = connection.models;
+// // // Nouvelle connexion à la base de données
+// // const connection = new Sequelize(
+// //     'autoeco', // Nom de la base de données
+// //     'root', // Identifiant MySQL
+// //     '', // Mot de passe MySQL
+// //     {
+// //         host: 'localhost', // URL de MySQL
+// //         dialect: 'mysql' // Type de base de données
+// //     }
+// // );
 
-// // Définition des relations entre les modèles
+// // // Authentification et connexion à la base de données
+// // try {
+// //     await connection.authenticate();
+// //     console.log('Connection has been established successfully.');
+// // } catch (error) {
+// //     console.error('Unable to connect to the database:', error);
+// // }
 
-// User.hasMany(Reservation);
-// Reservation.belongsTo(User);
+// // // Création des connexions aux tables via les modèles
+// //  carModel(connection, Sequelize);
+// //  carImageModel(connection, Sequelize);
+// // // carLocationModel(connection, Sequelize);
+// // // carTypeModel(connection, Sequelize);
+// //  paymentModel(connection, Sequelize);
+// //  reservationModel(connection, Sequelize);
+// //  userModel(connection, Sequelize);
 
-// Reservation.hasMany(Payment);
-// Payment.belongsTo(Reservation);
+// // // Récupération des modèles créés
+// // const {
+// //      Car,
+// //      Payment,
+// //      Reservation,
+// //      User,
+// //      CarImage
+// //       // CarImage,
+// //  // CarLocation,
+// //  // CarType,
+// // } = connection.models;
 
-// Payment.belongsTo(User);
-// User.hasMany(Payment);
+// // // Définition des relations entre les modèles
 
-// // Un Car peut avoir plusieurs CarImages
-// Car.hasMany(CarImage);
+// // User.hasMany(Reservation);
+// // Reservation.belongsTo(User);
+
+// // Reservation.hasMany(Payment);
+// // Payment.belongsTo(Reservation);
+
+// // Payment.belongsTo(User);
+// // User.hasMany(Payment);
+
+// // // Un Car peut avoir plusieurs CarImages
+// // Car.hasMany(CarImage);
   
-//   // Une CarImage appartient à un Car
-// CarImage.belongsTo(Car);
+// //   // Une CarImage appartient à un Car
+// // CarImage.belongsTo(Car);
   
   
 
@@ -390,21 +401,21 @@ export {
 
 
 
-// // Définition du port pour le serveur
-// const PORT = process.env.PORT || 8000
+// // // Définition du port pour le serveur
+// // const PORT = process.env.PORT || 8000
 
 
-// // Synchronisation des modèles avec la base de données
-// await connection.sync();
-// console.log('Synchro OK');
+// // // Synchronisation des modèles avec la base de données
+// // await connection.sync();
+// // console.log('Synchro OK');
 
-// // Exportation des modèles pour utilisation dans d'autres parties de l'application
-// export {
-//     Car,
-//   CarImage,
-// // CarLocation,
-// // CarType,
-//    Payment,
-//     Reservation,
-//     User
-// };
+// // // Exportation des modèles pour utilisation dans d'autres parties de l'application
+// // export {
+// //     Car,
+// //   CarImage,
+// // // CarLocation,
+// // // CarType,
+// //    Payment,
+// //     Reservation,
+// //     User
+// // };
